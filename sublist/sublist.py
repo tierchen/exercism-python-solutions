3820c9454c0853cb4643093efe11d2bcd23aceaf
Max Makhlai <maxmakhlai@gmail.com>
2018-02-06 14:48:31 +0500

    final speed-up of sublist implementation

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -2,13 +2,19 @@ SUBLIST, SUPERLIST, EQUAL, UNEQUAL = range(4)
 
 
 def check_lists(first_list, second_list):
-    first = '-'.join(map(str, first_list))
-    second = '-'.join(map(str, second_list))
-    if first == second:
+    def is_sub(sub_list, super_list):
+        if not sub_list:
+            return True
+        for i in range(len(super_list) - len(sub_list) + 1):
+            if sub_list[0] == super_list[i] and sub_list == super_list[i:i+len(sub_list)]:
+                return True
+        return False
+
+    if first_list == second_list:
         return EQUAL
-    elif first in second:
+    elif is_sub(first_list, second_list):
         return SUBLIST
-    elif second in first:
+    elif is_sub(second_list, first_list):
         return SUPERLIST
     else:
         return UNEQUAL
