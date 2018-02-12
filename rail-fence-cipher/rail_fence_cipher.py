aa01b3cb3a4a3f3bc9c3079091a767a98b3378e6
Max Makhlai <maxmakhlai@gmail.com>
2018-02-12 15:55:41 +0500

    remake of rail_fence_cipher problem

--- rail-fence-cipher/rail_fence_cipher.py
+++ rail-fence-cipher/rail_fence_cipher.py
@@ -1,40 +1,21 @@
-PH = '?'  # placeholder
+from itertools import cycle, chain
 
 
-def _rail(rails):
-    cur, inc = 0, -1
-    yield cur
-    while True:
-        if cur + 1 == rails or cur == 0:
-            inc = -inc
-        cur += inc
-        yield cur
+def fence_pattern(message_size, rails):
+    board = [[None]*message_size for _ in range(rails)]
+    indexes = cycle(chain(range(0, rails-1), range(rails-1, 0, -1)))
+    for i, j in zip(indexes, range(message_size)):
+        board[i][j] = j
+    return [x for row in board for x in row if x is not None]
 
 
 def encode(message, rails):
-    board = [[None]*len(message) for i in range(rails)]
-    for i, j, c in zip(_rail(rails), range(len(message)), message):
-        board[i][j] = c
-
-    result = []
-    for row in board:
-        result.extend([x for x in row if x is not None])
-
-    return ''.join(result)
+    return ''.join([message[i] for i in fence_pattern(len(message), rails)])
 
 
 def decode(encoded_message, rails):
-    board = [[None] * len(encoded_message) for i in range(rails)]
-    for i, j, c in zip(_rail(rails), range(len(encoded_message)), encoded_message):
-        board[i][j] = PH
-
-    message = list(reversed(encoded_message))
-    for row in board:
-        for i in range(len(row)):
-            if row[i] == PH:
-                row[i] = message.pop()
-
-    result = []
-    for i, j in zip(_rail(rails), range(len(encoded_message))):
-        result.append(board[i][j])
-    return ''.join(result)
+    indexes = fence_pattern(len(encoded_message), rails)
+    inverse_indexes = [None]*len(encoded_message)
+    for i, j in enumerate(indexes):
+        inverse_indexes[j] = i
+    return ''.join([encoded_message[i] for i in inverse_indexes])
