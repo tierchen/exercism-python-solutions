9de383396faea1e32ee4af68ba8954c2e33d914d
Max Makhlai <maxmakhlai@gmail.com>
2018-02-16 22:02:44 +0500

    Add phone-number problem

---
+++ phone-number/phone_number.py
@@ -0,0 +1,20 @@
+import re
+
+
+class Phone(object):
+    def __init__(self, phone_number):
+        match = re.match(r'1?([2-9]\d{2}[2-9]\d{6})$', ''.join([d for d in phone_number if d.isdigit()]))
+        if not match:
+            raise ValueError('Invalid phone number')
+        self.number = match.group(1)
+
+    @property
+    def area_code(self):
+        return self.number[:3]
+
+    @property
+    def local_number(self):
+        return self.number[3:]
+
+    def pretty(self):
+        return ''.join(['(', self.area_code, ') ', self.local_number[:3], '-', self.local_number[3:]])
