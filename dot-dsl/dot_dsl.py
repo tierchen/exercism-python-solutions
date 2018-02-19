a04ea8baa1739b7fb669ab73b73e14c364caad73
Max Makhlai <maxmakhlai@gmail.com>
2018-02-19 14:57:33 +0500

    Add dot-dsl problem

--- dot-dsl/dot_dsl.py
+++ dot-dsl/dot_dsl.py
@@ -1,20 +1,24 @@
 NODE, EDGE, ATTR = range(3)
 
 
-class Node(object):
-    def __init__(self, name, attrs={}):
+class Node:
+    def __init__(self, name, attrs=None):
         self.name = name
-        self.attrs = attrs
+        self.attrs = {}
+        if attrs is not None:
+            self.attrs = attrs
 
     def __eq__(self, other):
         return self.name == other.name and self.attrs == other.attrs
 
 
-class Edge(object):
-    def __init__(self, src, dst, attrs={}):
+class Edge:
+    def __init__(self, src, dst, attrs=None):
         self.src = src
         self.dst = dst
-        self.attrs = attrs
+        self.attrs = {}
+        if attrs is not None:
+            self.attrs = attrs
 
     def __eq__(self, other):
         return (self.src == other.src and
@@ -22,6 +26,20 @@ class Edge(object):
                 self.attrs == other.attrs)
 
 
-class Graph(object):
-    def __init__(self, data=[]):
-        pass
+class Graph:
+    def __init__(self, data=None):
+        data = data or []
+
+        if any(len(item) < 2 for item in data):
+            raise TypeError('malformed graph or item')
+
+        if any(len(params) != 2 for t, *params in data if t in [NODE, ATTR]) \
+                or any(len(params) != 3 for t, *params in data if t == EDGE):
+            raise ValueError('malformed item')
+
+        if {t for t, *_ in data} - {NODE, EDGE, ATTR}:
+            raise ValueError('unknown item')
+
+        self.nodes = [Node(*params) for t, *params in data if t == NODE]
+        self.edges = [Edge(*params) for t, *params in data if t == EDGE]
+        self.attrs = {attr: value for t, *params in data if t == ATTR for attr, value in [params]}
