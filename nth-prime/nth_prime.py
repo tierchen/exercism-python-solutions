67848dce946df83c342e061a15f3ce0f2775334d
Max Makhlai <maxmakhlai@gmail.com>
2018-02-25 23:49:08 +0500

    Add prime**2 < cur restriction

--- nth-prime/nth_prime.py
+++ nth-prime/nth_prime.py
@@ -5,12 +5,14 @@ def nth_prime(positive_number):
     if positive_number < 1:
         raise ValueError("There is no {}th prime".format(positive_number))
     primes = [2]
-    while len(primes) != positive_number:
-        for cur in count(primes[-1]+1):
-            for prime in primes:
-                if cur % prime == 0:
-                    break
-            else:
+
+    for cur in count(primes[-1]+1):
+        if len(primes) == positive_number:
+            return primes[-1]
+        stop = int(cur ** 0.5) + 1
+        for prime in primes:
+            if cur % prime == 0:
+                break
+            if prime >= stop:
                 primes.append(cur)
                 break
-    return primes[-1]
